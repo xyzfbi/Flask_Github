709d760fef685ae5b90e5497267e87fb291b7681
Першин Максим Дмитриевич <mdpershin@edu.hse.ru>
2025-03-08 20:16:18 +0300

    Updated resume_generator.py, added index.html, where storage web.
    В resume_generator.py добавлен возврат рендера в index.html

--- src/resume_generator.py
+++ src/resume_generator.py
@@ -1,10 +1,11 @@
 import os
 from src import github_api
+from jinja2 import Template
+from flask import render_template
 
 RESUME_FILE_PATH = os.path.join('data', 'resume.txt')
-
-
-
+INDEX_FILE_PATH = os.path.join('data', 'index.html')
+TEMPLATE_FILE_PATH = os.path.join('templates', 'index.html')
 
 def generate_resume(username):
     profile_stats = github_api.get_profile_stats(username)
@@ -23,6 +24,11 @@ def generate_resume(username):
         "followers": profile_stats.get('followers', 0),
         "following": profile_stats.get('following', 0)
     }
+
+    rendered_resume = render_template('index.html', data=data)
+
+    with open(INDEX_FILE_PATH, "w", encoding="utf-8") as resume_file:
+        resume_file.write(rendered_resume)
 
     save_resume(data)
     return data
